5089245337366e771bb52a72ae7627bf5f907047
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-11 17:36:46 +0300

    task 01 done

---
+++ task01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, проверяющая, является ли одно введенное число квадратом второго
+System.Console.WriteLine("Enter two numbers: ");
+int user_num_1 = Convert.ToInt32(Console.ReadLine());
+int user_num_2 = Convert.ToInt32(Console.ReadLine());
+
+if (user_num_1 / user_num_2 == user_num_2)
+{
+    System.Console.WriteLine("First number is a square of second one");
+}
+else {
+    System.Console.WriteLine("First number is not a square of second one");
+}
