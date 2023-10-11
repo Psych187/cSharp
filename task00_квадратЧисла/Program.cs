9bf50315e5677f51473e20fbb0998e26f32fc0cb
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-11 17:25:07 +0300

    SquareNumber task done

---
+++ task00_квадратЧисла/Program.cs
@@ -0,0 +1,5 @@
+﻿/* Программа для вывода квадрата числа*/
+System.Console.WriteLine("Enter number: "); // cw - быстрый вызов Console.WriteLine
+int userNumber = Convert.ToInt32(Console.ReadLine()); // считываем ввод с консоли, конвертируем в число типа int и присваиваем в переменную
+int result = userNumber * userNumber;
+Console.WriteLine(result);
