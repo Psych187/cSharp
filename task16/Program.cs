1fe5da2222f1af33d61f4887e42dd9f57c42e69c
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-12 17:45:40 +0300

    task16 done

---
+++ task16/Program.cs
@@ -0,0 +1,23 @@
+﻿// принимает на вход два числа и проверяет, является ли одно число квадратом другого
+System.Console.WriteLine("Enter number1: "); 
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter number2: "); 
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    System.Console.WriteLine($"Number `{number1}` is a square of number `{number2}`");
+}
+if (number2 == number1 * number1)
+{
+    System.Console.WriteLine($"Number `{number2}` is a square of number `{number1}`");
+}
+else 
+System.Console.WriteLine("There is no square");
+
+/*
+if (number1 == number2 * number2 || number2 == number1 * number1)
+{
+    System.Console.WriteLine($"One number is a square of second one");}
+else 
+System.Console.WriteLine("There is no square");
+*/
