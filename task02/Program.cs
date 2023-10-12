eb8a0fb7c9f85a3c91a55c4cea76c15b117b3f18
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-12 11:49:17 +0300

    task 02 done

---
+++ task02/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа, выводящая день недели (название) при вводе пользователем цифры дня недели
+System.Console.WriteLine("Enter number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber == 1) 
+{
+    System.Console.WriteLine("Monday");
+}
+if (userNumber == 2) 
+{
+    System.Console.WriteLine("Tuesday");
+}
+if (userNumber == 3)
+{
+    System.Console.WriteLine("Wednesday");
+}
+if (userNumber == 4) 
+{
+    System.Console.WriteLine("Thursday");
+}
+if (userNumber == 5) 
+{
+    System.Console.WriteLine("Friday");
+}
+if (userNumber == 6) 
+{
+    System.Console.WriteLine("Saturday");
+}
+if (userNumber == 7) 
+{
+    System.Console.WriteLine("Sunday");
+}
