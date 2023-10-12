fd9b6789315a86f7a6eede338cdfabb0350393b3
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-12 13:16:14 +0300

    task08 HT done

---
+++ task08HT/Program.cs
@@ -0,0 +1,5 @@
+﻿// Программа принимает на вход число и выводит все четные числа от 1 до этого числа включительно (если оно четное)
+System.Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= number; i+=2)
+{System.Console.Write(i + "\t");}
