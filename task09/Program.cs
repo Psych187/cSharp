bf5b198dc526cc4df605586d820fb84a4b145353
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-12 13:35:55 +0300

    task09 HT done

---
+++ task09/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Программа, выводящая случайное число из отрезка [10,99] и показывает его наибольшую цифру*/
+
+int number = new Random().Next(10,100);
+System.Console.WriteLine(number);
+
+int first = number / 10;
+int second = number % 10;
+if (first > second) {
+    System.Console.WriteLine("The biggest is " + first);
+}
+if (first < second) {
+    System.Console.WriteLine("The biggest is " + second);
+}
