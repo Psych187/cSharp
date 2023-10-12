17823589217a7daed79f20d55cc82bb980091a93
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-12 12:35:39 +0300

    task 07 done

---
+++ task07/Program.cs
@@ -0,0 +1,5 @@
+﻿// Программа принимает на вход 3х значное число, на выходе показывает его последнюю цифру
+ System.Console.WriteLine("Enter nuber: ");
+ int userNumber = Convert.ToInt32(Console.ReadLine());
+ int result = userNumber % 10;
+ System.Console.WriteLine(result);
