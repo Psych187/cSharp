05e860158bdf20b0ee2819cc97b77baaa3913ba2
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-12 13:43:42 +0300

    task11 done

---
+++ task11/Program.cs
@@ -0,0 +1,7 @@
+﻿// программа выводит случайное 3х значное число и удаляет вторую цифру
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine(number);
+ int first = number / 100;
+ int second = number % 10;
+ System.Console.Write(first + " " + second);
