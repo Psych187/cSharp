4a3a6bdc509b38c056d80ffacd42eb3826d87caf
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-12 13:10:20 +0300

    task06 HT done

---
+++ task06_HT/Program.cs
@@ -0,0 +1,9 @@
+﻿// Проверка четности вводимого числа
+System.Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($"Number `{number}` is even");
+}
+else 
+System.Console.WriteLine($"Number `{number}` is not even");
