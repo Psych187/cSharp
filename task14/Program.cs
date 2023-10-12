b004abe10bdf824e4b14b180a81f102fc15cc2f4
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-12 17:34:32 +0300

    task14 done

---
+++ task14/Program.cs
@@ -0,0 +1,10 @@
+﻿// Принимает на вход число и проверяет кратность 7 и 23
+System.Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    System.Console.WriteLine("multiple");
+}
+else {
+    System.Console.WriteLine("not multiple");
+}
