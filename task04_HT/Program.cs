3299fc981899be60d016701183273149f5b674ca
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-12 13:02:45 +0300

    task04 HT done

---
+++ task04_HT/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа для определения наибольшего из трех чисел
+System.Console.WriteLine("Enter number 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter number 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number2 > max) {
+    max = number2;
+}
+if (number3 > max) {
+    max = number3;
+}
+Console.WriteLine("Maximal number is " + max);
