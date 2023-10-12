8361da4e1f2d136eb3eb44980e583ab7ea3fa3f5
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-12 12:12:41 +0300

    task 05 done

--- task05/Program.cs
+++ task05/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Программа, принимающая на вход одно число N, а на выходе показывает все числа промежутка -N..N
+System.Console.WriteLine("Enter number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int start = numberA * -1;
+while (start <= numberA)
+{
+    Console.Write(start + " ");
+    start = start + 1;
+}
