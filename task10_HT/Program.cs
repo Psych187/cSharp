672eb26731679fba3f790d415ce3229089f937ce
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-13 12:07:41 +0300

    task10_HT done

---
+++ task10_HT/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+System.Console.WriteLine("Enter 3-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int first = number % 100;
+int second = first / 10;
+System.Console.Write(second);
