32db1cc5fc8a0a2289550705f4d85ed9b53b435e
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-13 12:12:15 +0300

    task10_HT done

--- task10_HT/Program.cs
+++ task10_HT/Program.cs
@@ -1,6 +1,11 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
 System.Console.WriteLine("Enter 3-digit number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int first = number % 100;
+if (number < 100 || number > 1000) {
+    Console.WriteLine("Error. Number does not match condition");
+}
+else {
+    int first = number % 100;
 int second = first / 10;
 System.Console.Write(second);
+}
