febafc7f85895f4a4d16f4d1862c5e0f3a8b87a9
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-18 10:28:39 +0300

    17 added

---
+++ task17/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход координаты точки (x&y) 
+и выдает номер четверти плоскости*/
+System.Console.WriteLine("Enter x: ");
+int userX = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter y: ");
+int userY = Convert.ToInt32(Console.ReadLine());
+
+if (userX > 0 && userY > 0)
+{
+    System.Console.WriteLine("it is first quarter");
+}
+if (userX < 0 && userY > 0)
+{
+    System.Console.WriteLine("it is second quarter");
+}
+if (userX < 0 && userY < 0)
+{
+    System.Console.WriteLine("it is third quarter");
+}
+if (userX > 0 && userY < 0)
+{
+    System.Console.WriteLine("it is fourth quarter");
+}
+if (userX == 0 || userY == 0)
+{
+    System.Console.WriteLine("it is axis");
+}
