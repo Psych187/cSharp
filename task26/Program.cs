1e157b9684f92b5d609e19511366ee9f620068f4
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-18 13:34:58 +0300

    26 added

---
+++ task26/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+Console.Clear();
+System.Console.WriteLine("enter number: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int Result(int number)
+{
+    int count = 0;
+    for (int i = number; i > 0; i /= 10)
+    {
+        count = count + 1;
+    }
+    return count;
+}
+int userCount = Result(userNum);
+System.Console.WriteLine(userCount);
