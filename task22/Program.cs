f6cacc256ea6444527bf23208fdd532ddd9d39f3
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-18 11:45:37 +0300

    22 added

---
+++ task22/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Принимает на вход число и выдает таблицу квадратов чисел 1..N*/
+Console.Clear();
+System.Console.Write("Enter number: ");
+int userNum = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= userNum; i++)
+{
+    System.Console.WriteLine($"{i} * {i} = {Math.Pow(i, 2)}");
+}
