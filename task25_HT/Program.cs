21f39d78df1b660bf35ae3ea9f192361f283dc36
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-19 10:13:07 +0300

    25 added

---
+++ task25_HT/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит 
+число A в натуральную степень B*/
+Console.Clear();
+
+System.Console.WriteLine("Enter number: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter degree: ");
+int userDegr = Convert.ToInt32(Console.ReadLine());
+
+if (userDegr < 0) {
+    System.Console.WriteLine("Degree cannot be less 0");
+} else
+System.Console.WriteLine(Math.Pow(userNum, userDegr));
