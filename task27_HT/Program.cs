7735e61e3cc05bb07c0ce3dc4eb7cb59196a0333
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-19 10:13:20 +0300

    27 added

---
+++ task27_HT/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе*/
+Console.Clear();
+
+System.Console.WriteLine("Enter number: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int SummNumbers(int a)
+{
+    int result = 0;
+    for (int i = a; i > 0; i /= 10)
+    {
+        result = i + result % 10;
+    }
+    return result;
+}
+int userResult = SummNumbers(userNum);
+System.Console.WriteLine($"{userNum} -> {userResult}");
