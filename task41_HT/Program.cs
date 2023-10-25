42ca978401910489c4524c283ae3706fcef843d2
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-25 14:39:05 +0300

    41, 43, 45 added

---
+++ task41_HT/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+Console.Clear();
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i]);
+        if (i < arr.Length - 1)
+            System.Console.Write(", ");
+    }
+    System.Console.WriteLine("]");
+}
+
+int userSize = Prompt("Enter array size: ");
+int[] userArray = GetArray(userSize);
+PrintArray(userArray);
+
+int CountPositivDigits(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result ++;
+    }
+    return result;
+}
+int count = CountPositivDigits(userArray);
+System.Console.WriteLine();
+System.Console.WriteLine($"Positive Digits > 0 = {count}");
---
+++ task45/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Напишите программу, которая будет создавать копию заданного одномерного массива 
+с помощью поэлементного копирования.*/
+Console.Clear();
+
+int[] GetRandArray(int size, int start, int end)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i]);
+        if (i < arr.Length - 1)
+            System.Console.Write(", ");
+    }
+    System.Console.Write("]");
+    System.Console.Write(" ");
+}
+
+System.Console.Write("Array size: ");
+int userSize = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Array diapazone from: ");
+int userStart = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Array diapazone till: ");
+int userEnd = Convert.ToInt32(Console.ReadLine());
+int[] userArray = GetRandArray(userSize, userStart, userEnd);
+PrintArray(userArray);
+
+Console.ForegroundColor = ConsoleColor.Green;
+System.Console.Write("-> ");
+
+int[] CopyArray(int[] array)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+PrintArray(CopyArray(userArray));
