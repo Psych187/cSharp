d281bd6474be7993bc44aab54acc4535289913cd
Irina Streltsova <bananaboss3000@gmail.com>
2023-10-26 11:44:12 +0300

    46, 48,49,51  added

---
+++ Array_Ind/Program.cs
@@ -0,0 +1,20 @@
+﻿// ВЫвод индексов
+void Print2DArray(int[,] array)
+{
+    System.Console.Write($"[ ]\t");
+    int start = 0;
+    for (int i = start; i < start + array.GetLength(1); i++)
+    {
+        System.Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.Write($"["+ i +"]\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
