f1430b2f9418bbdda4f92cb135af71f32ff8102b
Irina Streltsova <bananaboss3000@gmail.com>
2023-11-16 11:03:54 +0300

    63, 65, 67, 69 added

---
+++ task65/Program.cs
@@ -0,0 +1,20 @@
+﻿//65. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Clear();
+
+System.Console.WriteLine("Enter first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter last number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void intN(int first, int last)
+{
+    if (last >= first) 
+        {
+        System.Console.Write(first + " ");
+        intN(first + 1, last);
+    } 
+    else return;
+}
+
+intN(number1, number2);
