6e26b071d626994743f4027b8f21d86985a5e8ca
Irina Streltsova <bananaboss3000@gmail.com>
2023-11-20 12:09:53 +0300

    64, 66, 68 attestation added

---
+++ task64_Att/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+System.Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void intN(int number)
+{
+    if (number >= 1)
+    {
+        System.Console.Write(number + " ");
+        intN(number - 1);
+    }
+}
+
+intN(number);
---
+++ task66_Att/Program.cs
@@ -0,0 +1,20 @@
+﻿// 66. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+System.Console.WriteLine("Enter first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter last number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int sumNum(int first, int last)
+{
+    if (last >= first) 
+       return (last + sumNum(first, last - 1));
+       else return 0;
+}
+
+int result = sumNum(number1, number2);
+System.Console.WriteLine(result);
